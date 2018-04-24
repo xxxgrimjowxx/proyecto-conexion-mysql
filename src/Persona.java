4a22ca42bd0245e26fe75588190cd24038e32767
Juan <Juan@DESKTOP-5NU27OI>
2018-04-23 20:09:01 -0500

    se cre la clase persona y se agrega la sentencia sql para insertar

---
+++ src/Persona.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+import conexion.Conexion;
+import javax.swing.JOptionPane;
+import org.omg.CORBA.ORB;
+public class Persona extends PersonaApp.PersonaPOA {
+    
+    private ORB orb;
+    
+    @Override
+    public boolean insertarPersona(int id, String nombre, String apellido, String telefono, String direccion, int identificacion) {
+       
+        boolean resultado = false;
+        try {
+            String query = "insert into persona (id, nombre, apelllido, telefono, direccion, identificacion)" + "values ("+id+","+nombre+","+apellido+","+telefono+","+direccion+","+identificacion+")";
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ocurrio un error"+e.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public String consultarPersona(int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarPersona(int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean actualizarPersona(int id, String nombre, String apellido, String telefono, String direccion, int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String listarPersonas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
