46e9a901250bab2bc14d8a5116e9e2f10d3e9fcb
Juan <Juan@DESKTOP-5NU27OI>
2018-04-25 21:11:15 -0500

    TERMINAR EL METODO DE INSERTAR PERSONA CREAR EL FORMULARIO PARA CAPTURAR LOS DATOS DE LA PERSONA

--- src/Persona.java
+++ src/Persona.java
@@ -5,6 +5,7 @@ import org.omg.CORBA.ORB;
 public class Persona extends PersonaApp.PersonaPOA {
     
     private ORB orb;
+    Conexion conex = new Conexion();
     
     @Override
     public boolean insertarPersona(int id, String nombre, String apellido, String telefono, String direccion, int identificacion) {
@@ -12,6 +13,16 @@ public class Persona extends PersonaApp.PersonaPOA {
         boolean resultado = false;
         try {
             String query = "insert into persona (id, nombre, apelllido, telefono, direccion, identificacion)" + "values ("+id+","+nombre+","+apellido+","+telefono+","+direccion+","+identificacion+")";
+            
+             conex.conexion();
+             Statement st = conex.conex.createStatement();
+             int valor = st.executeUpdate(query);
+             if(valor > 0) {
+                 resultado = true;
+             }
+             //cerramos los recursos
+             st.close();
+             conex.conex.close();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "ocurrio un error"+e.getMessage());
         }
