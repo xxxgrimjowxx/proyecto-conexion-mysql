400c1636571d9bac50c223e9517c1174d53ca7ac
Juan <Juan@DESKTOP-5NU27OI>
2018-05-02 21:43:09 -0500

    SE INTSERTAN LOS METODOS ELIMINAR, ACTUALIZAR,CONSULTAR PERSONA

--- src/Persona.java
+++ src/Persona.java
@@ -31,23 +31,104 @@ public class Persona extends PersonaApp.PersonaPOA {
     }
 
     @Override
-    public String consultarPersona(int identificacion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String consultarPersona(int id) {
+        String resultado="";
+        try {
+            String query = "SELECT * FROM persona WHERE id=" + id;
+            conex.conexion();
+            Statement st=conex.conex.createStatement();
+            ResultSet rs= st.executeQuery(query);
+            while (rs.next()){
+                resultado+= rs.getLong(1)+" - "
+                        +rs.getString(2)+" - "
+                        +rs.getString(3)+" - "
+                        +rs.getLong(4)+" - "
+                        +rs.getString(5)+" - "
+                        +rs.getLong(6) +"\n";
+                        
+            }
+            //cerrramos los recursos
+            st.close();
+            rs.close();
+            conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ocurrio un error"+ e.getMessage());
+        }
+        return resultado;
     }
+    
 
     @Override
-    public boolean eliminarPersona(int identificacion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean eliminarPersona(int id) {
+             boolean resultado = false;
+        try {
+            String query = "DELETE FROM persona WHERE id="+id; 
+                conex.conexion();
+             Statement st = conex.conex.createStatement();
+             int valor = st.executeUpdate(query);
+             if(valor > 0) {
+                 resultado = true;
+                 JOptionPane.showMessageDialog(null, "dato eliminado");
+             }
+             //cerramos los recursos
+             st.close();
+             conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ocurrio un error"+e.getMessage());
+        }
+       
+        return resultado;
     }
 
     @Override
     public boolean actualizarPersona(int id, String nombre, String apellido, String telefono, String direccion, int identificacion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean resultado = false;
+        try {
+            String query = "update persona set nombre ='"+nombre+"', apellido ='"+apellido+"', telefono = '"+telefono+"', direccion ='"+direccion+"' , identificacion = '"+identificacion+"' where id ="+id; 
+                conex.conexion();
+             Statement st = conex.conex.createStatement();
+             int valor = st.executeUpdate(query);
+             if(valor > 0) {
+                 resultado = true;
+                 JOptionPane.showMessageDialog(null, "dato actualizado");
+             }
+             //cerramos los recursos
+             st.close();
+             conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ocurrio un error"+e.getMessage());
+        }
+       
+        return resultado;
+                
     }
 
     @Override
     public String listarPersonas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       String resultado="";
+        try {
+            String query = "SELECT * FROM persona";
+            conex.conexion();
+            Statement st=conex.conex.createStatement();
+            ResultSet rs= st.executeQuery(query);
+            while (rs.next()){
+                resultado+= rs.getLong(1)+" - "
+                        +rs.getString(2)+" - "
+                        +rs.getString(3)+" - "
+                        +rs.getLong(4)+" - "
+                        +rs.getString(5)+" - "
+                        +rs.getLong(6) +"\n";
+                        
+            }
+            //cerrramos los recursos
+            st.close();
+            rs.close();
+            conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ocurrio un error"+ e.getMessage());
+        }
+        return resultado;
+    
     }
 
     @Override
--- src/PersonaFrm.java
+++ src/PersonaFrm.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,6 +29,7 @@ public class PersonaFrm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtID = new javax.swing.JTextField();
@@ -40,6 +44,10 @@ public class PersonaFrm extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         txtIDENTIFICACION = new javax.swing.JTextField();
         btnINSERTAR = new javax.swing.JButton();
+        BTNLISTAR = new javax.swing.JButton();
+        BTNCONSULTAR = new javax.swing.JButton();
+        BTNACTUALIZAR = new javax.swing.JButton();
+        BTNBORRAR = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -95,14 +103,38 @@ public class PersonaFrm extends javax.swing.JFrame {
             }
         });
 
+        BTNLISTAR.setText("LISTAR");
+        BTNLISTAR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNLISTARActionPerformed(evt);
+            }
+        });
+
+        BTNCONSULTAR.setText("CONSULTAR");
+        BTNCONSULTAR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNCONSULTARActionPerformed(evt);
+            }
+        });
+
+        BTNACTUALIZAR.setText("ACTUALIZAR");
+        BTNACTUALIZAR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNACTUALIZARActionPerformed(evt);
+            }
+        });
+
+        BTNBORRAR.setText("BORRAR");
+        BTNBORRAR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNBORRARActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(158, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(155, 155, 155))
             .addGroup(layout.createSequentialGroup()
                 .addGap(45, 45, 45)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,48 +146,79 @@ public class PersonaFrm extends javax.swing.JFrame {
                     .addComponent(jLabel7))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnINSERTAR)
-                    .addComponent(txtDIRECCION, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtID, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(txtNOMBRE, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(txtAPELLIDO, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(txtTELEFONO, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(txtIDENTIFICACION, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtDIRECCION, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(37, 37, 37)
+                        .addComponent(BTNBORRAR))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtID, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtIDENTIFICACION, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(txtNOMBRE, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txtAPELLIDO, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txtTELEFONO, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(29, 29, 29)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(BTNCONSULTAR)
+                                    .addComponent(BTNLISTAR)
+                                    .addComponent(btnINSERTAR)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(60, 60, 60)
+                                .addComponent(BTNACTUALIZAR)))))
+                .addContainerGap(156, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(306, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(155, 155, 155))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(txtID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(txtIDENTIFICACION, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(txtNOMBRE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(txtAPELLIDO, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(txtTELEFONO, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(30, 30, 30)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(txtID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(7, 7, 7))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnINSERTAR)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel7)
+                            .addComponent(txtIDENTIFICACION, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(BTNLISTAR))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(txtNOMBRE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(BTNCONSULTAR))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(txtAPELLIDO, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(txtTELEFONO, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(160, 160, 160)
+                        .addComponent(BTNACTUALIZAR)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(txtDIRECCION, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
-                .addComponent(btnINSERTAR)
-                .addGap(23, 23, 23))
+                    .addComponent(txtDIRECCION, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BTNBORRAR))
+                .addContainerGap(168, Short.MAX_VALUE))
         );
 
         pack();
@@ -185,6 +248,29 @@ public class PersonaFrm extends javax.swing.JFrame {
         Persona per = new Persona();
         per.insertarPersona(Integer.parseInt(txtID.getText()), txtNOMBRE.getText(), txtAPELLIDO.getText(), txtTELEFONO.getText(), txtDIRECCION.getText(),Integer.parseInt (txtIDENTIFICACION.getText()));
     }//GEN-LAST:event_btnINSERTARActionPerformed
+
+    private void BTNLISTARActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNLISTARActionPerformed
+        Persona per= new Persona();
+        String lista= per.listarPersonas();
+        JOptionPane.showMessageDialog(null,lista);
+    }//GEN-LAST:event_BTNLISTARActionPerformed
+
+    private void BTNCONSULTARActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNCONSULTARActionPerformed
+        Persona per= new Persona();
+        int id =Integer.parseInt(txtID.getText());
+         String lista= per.consultarPersona(id);
+         JOptionPane.showMessageDialog(null,lista);
+    }//GEN-LAST:event_BTNCONSULTARActionPerformed
+
+    private void BTNACTUALIZARActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNACTUALIZARActionPerformed
+       Persona per= new Persona();
+       per.actualizarPersona( Integer.parseInt(txtID.getText()), txtNOMBRE.getText(), txtAPELLIDO.getText(), txtTELEFONO.getText(), txtDIRECCION.getText(), Integer.parseInt(txtIDENTIFICACION.getText()));
+    }//GEN-LAST:event_BTNACTUALIZARActionPerformed
+
+    private void BTNBORRARActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNBORRARActionPerformed
+       Persona per= new Persona();
+       per.eliminarPersona(Integer.parseInt(txtID.getText()));
+    }//GEN-LAST:event_BTNBORRARActionPerformed
 
     /**
      * @param args the command line arguments
@@ -222,7 +308,12 @@ public class PersonaFrm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BTNACTUALIZAR;
+    private javax.swing.JButton BTNBORRAR;
+    private javax.swing.JButton BTNCONSULTAR;
+    private javax.swing.JButton BTNLISTAR;
     private javax.swing.JButton btnINSERTAR;
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
